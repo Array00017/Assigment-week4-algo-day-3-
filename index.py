72f4b1307c46ef2602b5c6fd64f0ca0e4a1ec37a
Arnold <Array0824@gmail.com>
2023-12-30 03:10:18 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,34 @@
+#Exercises
+#Exercise #1
+#Write a function that takes a list of integers and returns the median value. *Can assume that the list of integers is sorted in ascending value*.
+
+def solution(data1):
+    nums = [2, 3, 5, 8, 9, 17]
+    nums.sort()
+    data1 = len(nums)
+    if data1 % 2 == 1
+    middle = len(data1 += data1 // 2)
+
+    return middle 
+
+
+#Exercise #2
+#Create a function that counts how many distinct words are in the string below, then outputs a dictionary with the words as the key and the value as the amount of times that word appears in the string.
+
+#Example Output:{'in': 1, 'computing': 1, 'a': 5, ...}
+
+a_text = 'In computing, a hash table hash map is a data structure in computing which implements an associative array abstract data type, a structure that can map keys to values. A hash table uses a hash function to compute an index into an array of buckets or slots from which the desired value can be found'
+ 
+def soulution(string1):
+    for x in string1 
+    string1.count(a_text{0,-1})
+    if string1.count(x)=> 0
+    return string1.count(x) + string1.count 
+
+#Exercise #3
+#Write a function implementing a Linear Search Algorithm. A linear search is a method for finding an element within a list. It sequentially checks each element of the list until a match is found or the whole list has been searched. If you do not find a match, return -1. You can assume the list is sorted
+
+def soulution(list1):
+    list7 = [2,4,5,6,8]
+    new = list1.search(list7(6))
+    print (solution(new))
